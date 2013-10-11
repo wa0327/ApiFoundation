d1a18c9025973ea9d4e0ba482c6c37e0bc53a64f
Jack Tsai <wa0327@gmail.com>
2013-10-11 15:40:54 +0800

    enable nuget package restore.

---
+++ source/ApiFoundation.WebApp/Web/Http/Tracing/TraceWriter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http.Tracing;
+
+namespace ApiFoundation.Web.Http.Tracing
+{
+    internal sealed class TraceWriter : ITraceWriter
+    {
+        public void Trace(HttpRequestMessage request, string category, TraceLevel level, Action<TraceRecord> traceAction)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
